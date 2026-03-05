d3e62c87d81b1a016eb9e70b7e051c5c157aa2b8
xueruolongjiasanqianzhan <2508314788@qq.com>
2026-03-05 15:28:44 +0800

    Fix NumPy alias compatibility for newer NumPy versions

--- utils/data_loaders.py
+++ utils/data_loaders.py
@@ -200,8 +200,8 @@ def rand_bbox(size, lam):
     W = size[3]
     H = size[4]
     cut_rat = np.sqrt(1. - lam)
-    cut_w = np.int(W * cut_rat)
-    cut_h = np.int(H * cut_rat)
+    cut_w = int(W * cut_rat)
+    cut_h = int(H * cut_rat)
 
     # uniform
     cx = np.random.randint(W)
